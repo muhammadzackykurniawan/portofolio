65353dd129fbf0593bb47805d2da528e6eacaa95
Zacky Kurniawan <77685152+muhammadzackykurniawan@users.noreply.github.com>
2022-09-27 19:35:06 +0700

    create index

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portofolio</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/wow/css/libs/animate.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container my-2">
+            <a class="navbar-brand text-black" href="#">Personal</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active text-warning" aria-current="page" href="#top">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-black" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-black" href="#services">Servis</a>
+                    </li>
+                    <li class="nav-item ms-2">
+                        <a class="btn btn-warning" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6 my-5 wow fadeInLeftBig">
+                <h2>Hi! I Am</h2>
+                <h2 class="text-warning">Zacky Kurniawan</h2>
+                <label>Welcome to Zacky Kurniawan We want our monthly roundup of new releases</label>
+                <a class="btn btn-warning m-3">Hire</a>
+            </div>
+            <div class="col-md-5 wow fadeInDownBig">
+                <img src="assets/img/bg1.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container my-5" id="services">
+        <div class="row">
+            <div class="col m-5">
+                <h3>My awesome</h3>
+                <h3 class="text-warning">services</h3>
+                <label>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis veniam quod earum excepturi debitis officia?</label>
+                <a class="btn btn-lg btn-warning m-3">Download CV</a>
+            </div>
+            <div class="col m-5">
+                <div class="card p-3 mx-auto" style="width: 18rem;">
+                    <div class="card-body text-center ">
+                        <img src="assets/img/programming.png" class="" alt="..." width="70px">
+                        <h5 class="card-title">Programming</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-white text-primary">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col">
+                <div class="card p-3" style="width: 18rem;">
+                    <div class="card-body text-center ">
+                        <img src="assets/img/paint-palette.png" class="" alt="..." width="70px">
+                        <h5 class="card-title">Design</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-white text-primary">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card p-3" style="width: 18rem;">
+                    <div class="card-body text-center ">
+                        <img src="assets/img/ux.png" class="" alt="..." width="70px">
+                        <h5 class="card-title">UI / UX</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-white text-primary">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-md-7 my-5 pt-5 text-center">
+                    <h3>Work for All This</h3>
+                    <h3 class="text-warning">Brand & Client</h3>
+                    <label>Lorem ipsum dolor sit amet consectetur.</label>
+                    <br>
+                    <a href="#" class="btn btn-warning mt-3">Detail</a>
+                </div>
+                <div class="col-md-5 my-5 py-5" style="background-image: url('assets/img/circle.png'); background-repeat: no-repeat; background-size: 80%;">
+                    <div class="row">
+                        <div class="col">
+                            <img class="me-5 my-5 rounded-circle img-thumbnail" src="assets/img/shopify.png" alt="" width="50px">
+                            <img class="me-5 my-5" src="assets/img/chrome.png" alt="" width="50px">
+                            <img class="my-5 rounded-circle img-thumbnail" src="assets/img/amazon-pay.png" alt="" width="50px">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <img class="me-5 mb-5" src="assets/img/git.png" alt="" width="50px">
+                            <img class="me-5 mb-5" src="assets/img/dropbox.png" alt="" width="50px">
+                            <img class="mb-5 rounded-circle img-thumbnail" src="assets/img/gitlab.png" alt="" width="50px">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <img class="me-5 rounded-circle img-thumbnail" src="assets/img/shopify.png" alt="" width="50px">
+                            <img class="me-5 rounded-circle img-thumbnail" src="assets/img/skype.png" alt="" width="50px">
+                            <img src="assets/img/firefox.png" alt="" width="50px">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="text-center">
+            <h2>Recent Project</h2>
+            <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nihil aliquam officiis? Eius modi vitae sint! Molestias perferendis enim ab aut placeat nihil expedita rem, officiis cum consequatur totam commodi.</label>
+        </div>
+
+        <div class="row mx-auto my-5 justify-content-center">
+            <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img"> <img src="assets/img/p1.jpg" class="img-thumbnail"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img"> <img src="assets/img/p2.jpg" class="img-thumbnail"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img"> <img src="assets/img/p3.jpg" class="img-thumbnail"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img"> <img src="assets/img/p4.jpg" class="img-thumbnail"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-img"> <img src="assets/img/p5.jpg" class="img-thumbnail"> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> </a> <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> </a>
+            </div>
+        </div>
+        <!-- <div id="carouselExampleFade" class="carousel slide carousel-fade my-5" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="slide">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="assets/img/p1.jpg" alt="" class="img-thumbnail" width="400px">
+                            </div>
+                            <div class="col-md-4">
+                                <img src="assets/img/p2.jpg" alt="" class="img-thumbnail" width="400px">
+                            </div>
+                            <div class="col-md-4">
+                                <img src="assets/img/p3.jpg" alt="" class="img-thumbnail" width="400px">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="slide">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="assets/img/p4.jpg" alt="" class="img-thumbnail" width="400px">
+                            </div>
+                            <div class="col-md-4">
+                                <img src="assets/img/p5.jpg" alt="" class="img-thumbnail" width="400px">
+                            </div>
+                            <div class="col-md-4">
+                                <img src="assets/img/p1.jpg" alt="" class="img-thumbnail" width="400px">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div> -->
+    </div>
+    <div class="container my-5" id="contact">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="text-center pt-4 col-md-8 penutup">
+                <h2>Get A Project? Let's Talk</h2>
+                <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nihil aliquam officiis? Eius modi vitae sint! Molestias perferendis enim ab aut placeat nihil expedita rem, officiis cum consequatur totam commodi.</label>
+                <a class="btn btn-warning m-3">Hire</a>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="slide.js"></script>
+    <script src="assets/wow/dist/wow.min.js"></script>
+    <script type="text/javascript">
+        wow = new WOW({
+            boxClass: 'wow', // default
+            animateClass: 'animated', // default
+            offset: 0, // default
+            mobile: true, // default
+            live: true // default
+        })
+        wow.init();
+    </script>
+</body>
+
+</html>
